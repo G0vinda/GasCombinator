4e64cff796f154daa4a30d6c2264edd3941fdfdc
bene-labs <62158116+bene-labs@users.noreply.github.com>
2023-04-18 13:05:50 +0200

    add GameState Manager

---
+++ Assets/GameStates.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStates : MonoBehaviour
+{
+    // Update is called once per frame
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
