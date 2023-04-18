1a801c26bf35f057b6f89bca09b415000bddf1ff
bene-labs <62158116+bene-labs@users.noreply.github.com>
2023-04-18 15:36:33 +0200

    fix BeanSpawner, Add MainScene

--- Assets/Scripts/Bean/BeanSpawner.cs
+++ Assets/Scripts/Bean/BeanSpawner.cs
@@ -20,7 +20,7 @@ namespace Bean
         protected void SpawnBean()
         {
             Instantiate(beanPrefabs[Random.Range(0, beanPrefabs.Length)], new Vector3(Random.Range(lowerLimit.position.x, upperLimit.position.x),
-                Random.Range(lowerLimit.position.y, upperLimit.position.y), lowerLimit.position.z), Quaternion.identity);
+                lowerLimit.position.y, Random.Range(lowerLimit.position.z, upperLimit.position.z)), Quaternion.identity);
             Invoke(nameof(SpawnBean), Random.Range(minSpawnDelay, maxSpawnDelay));
         }
     }
